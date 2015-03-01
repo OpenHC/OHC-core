d3f400351d7f97246428fe4d12d15cf55ab5b728
Tobias <tobleminer@gmail.com>
2015-03-01 14:25:16 +0100

    First sketch for new packet format

--- src/core_remote.h
+++ src/core_remote.h
@@ -20,25 +20,55 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 
-Data packet:
-	[0..4]					Address
-	[5.5, 5.1, 5.1, 5.1]	Data len, Operation, Continuation, Last packet
-	[6..7]					Field index
-	[8..31]					Data
+Init packet:
+	[0]						Type indicator (0 for transmission start)
+	[1..5]					Address
+	[6..8]					Response challenge
+	[9..10]					Number of packets
+	[11]					Operation
+	[12..13]				Field index
 	
 	Operation = 0 => read
 	Operation = 1 => write
+	
+Connection ACK packet
+	[0]						Type indicator (100 for connection ACK packet)
+	[1..3]					Challenge
+	[4..6]					Response challenge
+	
+Connection NAK packet
+	[0]						Type indicator (101 for connection NAK packet)
+	[1..3]					Challenge
+	[4..6]					Response challenge
+	
+Data packet
+	[0]						Type indicator (1 for data packet)
+	[1..3]					Challenge
+	[4..6]					Response challenge
+	[7]						Packet length
+	[8..31]					Data
 */
 
 #ifndef __OHC_CORE_REMOTE_H__
 #define __OHC_CORE_REMOTE_H__
 	
+	//Typedef
+	typedef struct
+	{
+		uint8_t*	my_challenge;
+		uint8_t*	remote_challenge;
+		uint16_t	packet_counter;
+		uint16_t	length;
+	}
+	ohc_core_remote_connection_t;
+
 	//Exit codes
 	#define CORE_REMOTE_ERROR_ADDR_TOO_SHORT	100
 	#define CORE_REMOTE_ERROR_ADDR_TOO_LONG		101
 	#define CORE_REMOTE_ERROR_PAYLOAD_TOO_LONG	102
 	
 	#define CORE_REMOTE_PACKET_OVERHEAD			8
+	#define CORE_REMOTE_CHALLENGE_LENGTH		3
 		
 	#if WIRELESS_PACK_LEN <= CORE_REMOTE_PACKET_OVERHEAD
 		#error "NRF24L01 packet too short to store any data"
